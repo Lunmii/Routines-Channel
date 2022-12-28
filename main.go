7f9a9aa0a3b9c15e02c5158baca29098be082744
Alo Oluwapelumi David <pelumidavid123456@gmail.com>
2022-12-28 21:05:08 +0100

    adding channels

--- main.go
+++ main.go
@@ -7,23 +7,30 @@ import (
 
 func main() {
 	links := []string{
-		"http://google.com",
-		"http://golang.org",
 		"http://amazon.com",
+		"http://golang.org",
+		"http://google.com",
 	}
+
+	c := make(chan string)
 
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
 
+	for l := range c {
+		go checkLink(l, c)
+	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
